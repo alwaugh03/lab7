2f8aa7e8f5b5849720b4435315b6c129f2d6b133
alwaugh03 <awaugh@miuandes.cl>
2026-04-16 09:56:19 -0400

    test lab 4

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,11 @@ class Appointment < ApplicationRecord
   belongs_to :pet
   belongs_to :vet
   has_many :treatments
+
+  enum :status, {
+    scheduled: 0,
+    completed: 1,
+    cancelled: 2,
+    pending: 3
+  }
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,4 @@
 class Pet < ApplicationRecord
   belongs_to :owner
-  has_many :appointmentss
+  has_many :appointments
 end
